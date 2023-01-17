afad3f8d877599a74015349ce5e41aefea7c3c9f
TrickEye <trickeye@buaa.edu.cn>
2023-01-17 23:07:31 +0800

    remove useless imports

--- md2latex_converter/__main__.py
+++ md2latex_converter/__main__.py
@@ -1,5 +1,5 @@
 from md2latex_converter.core.cmd_parser import parse_command, Cmd
-
+from md2latex_converter.data_structures.extensions import SentExt
 
 def main() -> None:
     # ext = SentExt('Code', r'```(?P<lang>.*)')
